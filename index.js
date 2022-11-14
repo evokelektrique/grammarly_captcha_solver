ab47cac90365379134162e571a5f1326026838cc
EVOKE <evoke.lektrique@gmail.com>
2022-11-14 12:15:34 +0330

    Add list account and fail operation handler

--- index.js
+++ index.js
@@ -1,71 +1,138 @@
 const fs = require("fs");
+const path = require("path");
 const puppeteer = require("puppeteer-extra");
 const StealthPlugin = require("puppeteer-extra-plugin-stealth");
 puppeteer.use(StealthPlugin());
 const { executablePath } = require("puppeteer");
+const { exit } = require("process");
+
+const list_path = path.resolve("./list.txt");
+const plugin_path = path.resolve("./plugin/js/config_ac_api_key.js"); // Convert relative to absolute path
+const global_args = [
+    "--disable-web-security",
+    "--disable-features=IsolateOrigins,site-per-process",
+    "--allow-running-insecure-content",
+    "--disable-blink-features=AutomationControlled",
+    "--no-sandbox",
+    "--mute-audio",
+    "--no-zygote",
+    "--no-xshm",
+    "--window-size=1920,1080",
+    "--no-first-run",
+    "--no-default-browser-check",
+    "--disable-dev-shm-usage",
+    "--disable-gpu",
+    "--enable-webgl",
+    "--ignore-certificate-errors",
+    "--lang=en-US,en;q=0.9",
+    "--password-store=basic",
+    "--disable-gpu-sandbox",
+    "--disable-software-rasterizer",
+    "--disable-background-timer-throttling",
+    "--disable-backgrounding-occluded-windows",
+    "--disable-renderer-backgrounding",
+    "--disable-infobars",
+    "--disable-breakpad",
+    "--disable-canvas-aa",
+    "--disable-2d-canvas-clip-aa",
+    "--disable-gl-drawing-for-tests",
+    "--enable-low-end-device-mode",
+    "--disable-extensions-except=./plugin",
+    "--load-extension=./plugin",
+    "--proxy-server=127.0.0.1:17000", // Proxy server
+];
 
 // Set api key
 const apiKey = "";
-if (fs.existsSync("./plugin/js/config_ac_api_key.js")) {
-    let confData = fs.readFileSync("./plugin/js/config_ac_api_key.js", "utf8");
+if (fs.existsSync(plugin_path)) {
+    let confData = fs.readFileSync(plugin_path, "utf8");
     confData = confData.replace(
         /antiCapthaPredefinedApiKey = ''/g,
         `antiCapthaPredefinedApiKey = '${apiKey}'`
     );
-    fs.writeFileSync("./plugin/js/config_ac_api_key.js", confData, "utf8");
+    fs.writeFileSync(plugin_path, confData, "utf8");
 } else {
     console.error("Plugin configuration not found!");
 }
 
+// Read the list
+const list_data = fs.readFileSync(list_path, { encoding: "utf-8" });
+list = generate_list(list_data);
+
 // Main stuff
 (async () => {
-
     // Init browser
     const browser = await puppeteer.launch({
         headless: false,
         executablePath: executablePath(),
         ignoreDefaultArgs: ["--disable-extensions", "--enable-automation"],
-        args: [
-            "--disable-web-security",
-            "--disable-features=IsolateOrigins,site-per-process",
-            "--allow-running-insecure-content",
-            "--disable-blink-features=AutomationControlled",
-            "--no-sandbox",
-            "--mute-audio",
-            "--no-zygote",
-            "--no-xshm",
-            "--window-size=1920,1080",
-            "--no-first-run",
-            "--no-default-browser-check",
-            "--disable-dev-shm-usage",
-            "--disable-gpu",
-            "--enable-webgl",
-            "--ignore-certificate-errors",
-            "--lang=en-US,en;q=0.9",
-            "--password-store=basic",
-            "--disable-gpu-sandbox",
-            "--disable-software-rasterizer",
-            "--disable-background-timer-throttling",
-            "--disable-backgrounding-occluded-windows",
-            "--disable-renderer-backgrounding",
-            "--disable-infobars",
-            "--disable-breakpad",
-            "--disable-canvas-aa",
-            "--disable-2d-canvas-clip-aa",
-            "--disable-gl-drawing-for-tests",
-            "--enable-low-end-device-mode",
-            "--disable-extensions-except=./plugin",
-            "--load-extension=./plugin",
-            "--proxy-server=127.0.0.1:17000", // Proxy server
-        ],
+        args: global_args,
     });
 
+    const page = await browser.newPage();
+
+    list.forEach(async (credential) => {
+        await retry(() => {
+            operation(page, credential);
+        }, 3);
+    });
+})();
+
+/**
+ *
+ * @param {Array} promiseFactory List of promises
+ * @param {Integer} retryCount Revolution counter
+ *
+ * @returns mixed
+ */
+async function retry(promiseFactory, retryCount) {
+    try {
+        return await promiseFactory();
+    } catch (error) {
+        if (retryCount <= 0) {
+            throw error;
+        }
+        return await retry(promiseFactory, retryCount - 1);
+    }
+}
+
+/**
+ *
+ * @param {String} data Given list data
+ *
+ * @returns Array of list
+ */
+function generate_list(data) {
+    if (data === "" || data === undefined) {
+        console.log("empty list given");
+    }
+
+    const list = [];
+
+    data = data.split("\n");
+    data.forEach((item) => {
+        const temp_item = item.split(":");
+        const email = temp_item[0];
+        const password = temp_item[1];
+
+        list.push({ email, password });
+    });
+
+    return list;
+}
+
+/**
+ * Solve the grammarly captcha
+ *
+ * @param {Puppeteer} page Browser tab
+ * @param {Object} credential Account credential
+ */
+async function operation(page, credential) {
     // Credentials
-    const email = "";
-    const password = "";
+    const email = credential.email;
+    const password = credential.password;
 
     // Open Grammarly login page
-    const page = await browser.newPage();
     const url = "https://www.grammarly.com/signin";
 
     try {
@@ -109,13 +176,21 @@ if (fs.existsSync("./plugin/js/config_ac_api_key.js")) {
 
     // Solve captcha
     await page
-        .waitForSelector(".antigate_solver.solved")
-        .catch((error) => console.log("failed to wait for the selector"));
+        .waitForSelector(".antigate_solver.solved", { timeout: 40000 }) // 40s timeout
+        .catch(async () => {
+            console.log("failed to wait for the selector");
+            throw new Error("failed to solve the captcha");
+        });
     console.log("recaptcha solved");
 
     // Wait for redirect
     await page.waitForNavigation("networkidle2");
 
     // Check if redirected url is correct
-    console.log(page.url() === "https://app.grammarly.com/");
-})();
+    if (page.url() === "https://app.grammarly.com/") {
+        console.log("Operation done");
+    }
+
+    // Close the tab
+    await page.close();
+}
